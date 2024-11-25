c0a68cf5bbc68c958822c7217e99a103443066f3
Yorke Rhodes <yorke@hyperlane.xyz>
2024-11-25 16:16:43 -0500

    Quick deploy enroll verify pipeline

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+# deploy.sh <CHAIN>
+CHAIN=$1
+
+THE_COMPACT_ADDRESS=0x00000000000018DF021Ff2467dF97ff846E09f48
+MAILBOX_ADDRESS=$(cat ~/.hyperlane/chains/$CHAIN/addresses.yaml | yq '.mailbox')
+RPC_URL=$(cat ~/.hyperlane/chains/$CHAIN/metadata.yaml | yq '.rpcUrls[0].http')
+HYP_KEY=$(gcloud secrets versions access latest --secret "hyperlane-mainnet3-key-deployer" | jq ".privateKey" -c -r)
+ARBITER_ADDRESS=$(forge create HyperlaneArbiter --json \
+    --rpc-url $RPC_URL \
+    --private-key $HYP_KEY \
+    --constructor-args $MAILBOX_ADDRESS $THE_COMPACT_ADDRESS \
+    | jq -r '.deployedTo')
+echo "$CHAIN: $ARBITER_ADDRESS" >> hyperlane-arbiters.yaml
