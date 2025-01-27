5f7e43c9bca29b88a833d98bc12002fded6085af
Yorke Rhodes <yorke@hyperlane.xyz>
2025-01-27 11:31:56 -0500

    Deploy HyperlaneTribunal

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ THE_COMPACT_ADDRESS=0x00000000000018DF021Ff2467dF97ff846E09f48
 MAILBOX_ADDRESS=$(cat ~/.hyperlane/chains/$CHAIN/addresses.yaml | yq '.mailbox')
 RPC_URL=$(cat ~/.hyperlane/chains/$CHAIN/metadata.yaml | yq '.rpcUrls[0].http')
 HYP_KEY=$(gcloud secrets versions access latest --secret "hyperlane-mainnet3-key-deployer" | jq ".privateKey" -c -r)
-ARBITER_ADDRESS=$(forge create HyperlaneArbiter --json \
+ARBITER_ADDRESS=$(forge create HyperlaneTribunal --json --broadcast \
     --rpc-url $RPC_URL \
     --private-key $HYP_KEY \
     --constructor-args $MAILBOX_ADDRESS $THE_COMPACT_ADDRESS \
